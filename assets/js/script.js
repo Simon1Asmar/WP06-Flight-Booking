e1ffe4c915d608bad01ebfaf8aadb34eeda79d72
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-11-04 10:17:29 +0200

    basic setup

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+const flights = [
+  {
+      from: "Tel aviv",
+      to:'amsterdam',
+      price: 40,
+      dates:[
+          {depart: new Date ('24.11.2023')},
+          {return: new Date ('1.12.2023')}
+      ]
+  },
+  {
+      from: "Tel aviv",
+      to:'london',
+      price: 75,
+      dates:[
+          {depart: new Date ('28.11.2023')},
+          {return: new Date ('12.12.2023')}
+      ]
+  },
+  {
+      from: "Athens",
+      to:'Prague',
+      price: 95,
+      dates:[
+          {depart: new Date ('28.11.2023')},
+          {return: new Date ('12.12.2023')}
+      ]
+  },
+  {
+      from: "Berlin",
+      to:'Prague',
+      price: 22,
+      dates:[
+          {depart: new Date ('28.11.2023')},
+          {return: new Date ('12.12.2023')}
+      ]
+  },
+  {
+      from: "London",
+      to:'Berlin',
+      price: 100,
+      dates:[
+          {depart: new Date ('28.11.2023')},
+          {return: new Date ('12.12.2023')}
+      ]
+  }
+]
