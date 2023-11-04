3d5d3ccafab8555e8b53a7b18e543ffb40827616
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-11-04 20:26:32 +0200

    basic login and logout

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,91 @@ const flights = [
       ]
   }
 ]
+
+//selecting DOM elements
+const loginForm = document.querySelector("#login-form");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const nameInput = document.querySelector("#name");
+const adminCheckbox = document.querySelector("#admin");
+
+let isAdmin = false;
+
+loginForm.addEventListener("submit", loginValidation);
+
+checkIfLoggedIn();
+
+//created logout button, to be appended or removed depending on wether the useris logged in or out
+const logoutButton = document.createElement("button");
+logoutButton.textContent = "Logout";
+logoutButton.addEventListener("click", logout);
+
+function loginValidation(event){
+  event.preventDefault();
+
+  let isValid = true;
+
+  //validate email
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(!emailRegex.test(emailInput.value)){
+    isValid = false;
+    showError(emailInput, "Please enter a valid email.");
+  } else {
+    showError(emailInput, "");
+  }
+
+  //validate password
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+  if(passwordRegex.test(passwordInput.value) || passwordInput.value.length === 0){
+    isValid = false;
+    showError(passwordInput, "Please enter a valid password.")
+  }else{
+    showError(passwordInput, "")
+  }
+
+
+  if(isValid){
+    console.log("valid");
+    isAdmin = adminCheckbox.checked;
+    let email = emailInput.value;
+    let name = nameInput.value;
+    let password = passwordInput.value;
+
+
+    localStorage.setItem("email", email);
+    localStorage.setItem("name", name);
+    localStorage.setItem("password", password);
+    localStorage.setItem("isAdmin", isAdmin);
+
+    login();
+    
+  }
+}
+
+function showError(input, message){
+  const errorP = document.querySelector(`#${input.id}Error`);
+  errorP.textContent = message;
+}
+
+function login(){
+  loginForm.remove();
+
+  document.body.append(logoutButton);
+}
+
+function logout(){
+  localStorage.clear();
+  logoutButton.remove();
+
+  document.body.append(loginForm);
+
+}
+
+function checkIfLoggedIn(){
+  console.log(localStorage);
+  if(localStorage.length > 0){
+    login();
+  } else {
+    logout();
+  }
+}
